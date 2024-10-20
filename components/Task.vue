d324f876584ffc7cf5ac6268c201254fc43d550e
TPilate <peroron.tm@gmail.com>
2024-10-20 21:53:25 +0200

    add new component and style

---
+++ components/Task.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="task-container">
+    <form class="task-container__form">
+      <label class="task-container__name" for="t-name">Task Name:</label>
+      <input class="task-container__input" type="text" id="t-name"></input>
+      <label class="task-container__name" for="t-description">Task description:</label>
+      <textarea class="task-container__input-description" type="text" id="t-description"></textarea>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
